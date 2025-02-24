324a5871753ca6f41fbd81e1232a74602f898ab0
Raj-0789 <reporttrick63@gmail.com>
2025-02-24 19:22:59 +0530

    Update hack.js

--- Priyansh/commands/hack.js
+++ Priyansh/commands/hack.js
@@ -87,7 +87,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
 	  
 	  
 	  const lines = await this.wrapText(ctx, name, 1160);
-	  ctx.fillText(lines.join('\n'), 100,330);//comment
+	  ctx.fillText(lines.join('\n'), 125,330);//comment
 	  ctx.beginPath();
 
 
